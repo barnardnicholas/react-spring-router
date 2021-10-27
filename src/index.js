a2a34ecbeddb9a56495a3072e75e230210cf6a40
Nick Barnard <barnard.nicholas@googlemail.com>
2021-10-27 14:57:50 +0100

    Fix bug with missing units on zero values in config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
-import { useTransition, Transition, animated, config } from 'react-spring';
+import { useTransition, animated, config } from 'react-spring';
 import Header from './components/Header';
 import ArnieIpsum from './containers/ArnieIpsum';
 import DeloreanIpsum from './containers/DeloreanIpsum';
@@ -15,28 +15,27 @@ const customConfig = {
   friction: 30,
 };
 
-const AnimatedRoute = ({ children }) => {
+const AnimatedRoute = ({ style, children }) => {
   const { pathname } = useLocation();
 
   const transitions = useTransition(pathname, {
+    key: pathname,
     from: { opacity: 0, transform: 'translateX(101vw) translateY(50vh) perspective(900px) rotateY(90deg)' },
-    enter: { opacity: 1, transform: 'translateX(0) translateY(0) perspective(900px) rotateY(0deg)' },
+    enter: { opacity: 1, transform: 'translateX(0vw) translateY(0vh) perspective(900px) rotateY(0deg)' }, // IMPORTANT - 0 values must have units
     leave: {
       opacity: 0,
       transform: 'translateX(-101vw) translateY(50vh) perspective(900px) rotateY(-90deg)',
       pointerEvents: 'none',
     },
-    config: customConfig,
+    config: config.stiff,
   });
 
   return (
     <Route
       render={({ location }) =>
-        transitions((style) => {
-          return (
-            <animated.div style={{ ...style, position: 'absolute', width: '100%' }}>{children(location)}</animated.div>
-          );
-        })
+        transitions((style) => (
+          <animated.div style={{ ...style, position: 'absolute', width: '100%' }}>{children(location)}</animated.div>
+        ))
       }
     />
   );
