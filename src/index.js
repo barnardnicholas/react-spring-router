b600e1356d3f37209fb6b979ed0d52b8c71c894c
Nick Barnard <barnard.nicholas@googlemail.com>
2021-11-23 21:32:14 +0000

    Remove animatedRoutes

--- src/index.js
+++ src/index.js
@@ -7,22 +7,17 @@ import DeloreanIpsum from './containers/DeloreanIpsum';
 import BaconIpsum from './containers/BaconIpsum';
 import './styles.css';
 import DemoPage from './containers/DemoPage';
-import AnimatedRoute from './AnimatedRoute';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <AnimatedRoute>
-        {(location) => (
-          <Switch location={location}>
-            <Route exact path="/arnie" render={(props) => <ArnieIpsum />} />
-            <Route path="/bacon" render={(props) => <BaconIpsum />} />
-            <Route path="/delorean" render={(props) => <DeloreanIpsum />} />
-            <Route path="/demo-page" render={(props) => <DemoPage />} />
-          </Switch>
-        )}
-      </AnimatedRoute>
+      <Switch>
+        <Route exact path="/arnie" render={(props) => <ArnieIpsum />} />
+        <Route path="/bacon" render={(props) => <BaconIpsum />} />
+        <Route path="/delorean" render={(props) => <DeloreanIpsum />} />
+        <Route path="/demo-page" render={(props) => <DemoPage />} />
+      </Switch>
     </Router>
   );
 };
