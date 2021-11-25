2afca070fe24f0377e1e8a7ff1f993a310508e8d
Nick Barnard <barnard.nicholas@googlemail.com>
2021-11-25 15:48:29 +0000

    Add SVG mouse follower

--- src/components/svg/SVGCurvedArrow.js
+++ src/components/svg/SVGCurvedArrow.js
@@ -1,21 +1,18 @@
 import { useState, useRef, useLayoutEffect } from 'react';
 import SVG from './SVG';
-import Nodes from './Nodes';
 import PathCurve from './PathCurve';
-import { calcAngle } from './utils';
 import PolygonTriangle from './PolygonTriangle';
 
 function SVGCurvedArrow({
+  position = { pX: 0, pY: 0 }, // percentage
   flipHorizontal = false,
   pointerLength = 30,
   pointerWidth = 15,
   up = false,
   left = false,
-  horizontalOnly = true,
-  verticalOnly = false,
-  cp,
 }) {
   const containerRef = useRef(document.createElement('div'));
+  console.log({ left: top });
 
   const [clientHeight, setClientHeight] = useState(0);
   const [clientWidth, setClientWidth] = useState(0);
@@ -27,23 +24,28 @@ function SVGCurvedArrow({
     }
   }, [containerRef.current.clientHeight, containerRef.current.clientWidth]);
 
+  const convertPercentToCoords = (pX, pY) => {
+    const x = (clientWidth / 100) * pX;
+    const y = (clientHeight / 100) * pY;
+    return [x, y];
+  };
+
   const svgProps = {
     width: clientWidth,
     height: clientHeight,
     customStyles: { flex: 1 },
   };
 
+  const startXY = [0, 0];
+  const endXY = convertPercentToCoords(position.pX, position.pY);
+  const centerXY = convertPercentToCoords(position.pX / 2, position.pY / 2);
+  const curveXY = convertPercentToCoords(position.pX / 3, 0);
+
   const curveProps = {
-    startXY: [flipHorizontal ? clientWidth - 10 : 10, 10],
-    endXY: [
-      left ? pointerLength + 10 : clientWidth - pointerLength - 10,
-      up ? pointerLength + 10 : clientHeight - pointerLength - 10,
-    ],
-    centerXY: [
-      (clientWidth - pointerLength) / 2 + (clientWidth - pointerLength) * cp.centerXY[0],
-      (clientHeight - pointerLength) / 2 + (clientHeight - pointerLength) * cp.centerXY[1],
-    ],
-    curveXY: [clientWidth / 3 + (clientWidth / 3) * cp.curveXY[0], 10 + (clientHeight / 3) * cp.curveXY[1]],
+    startXY,
+    endXY,
+    centerXY,
+    curveXY,
     strokeWidth: 3,
     strokeColor: '#f7f7f7',
     customStyles: {},
@@ -53,13 +55,12 @@ function SVGCurvedArrow({
     startXY: [curveProps.endXY[0], curveProps.endXY[1]],
     width: pointerLength,
     height: pointerWidth,
-    angle: 0,
+    angle: left ? 180 : 0,
   };
 
   const containerStyle = {
     width: '100%',
     height: '100%',
-    border: cp.showBorder ? '1px solid red' : 'unset',
   };
 
   return (
@@ -67,7 +68,6 @@ function SVGCurvedArrow({
       <SVG {...svgProps}>
         <PathCurve {...curveProps} />
         <PolygonTriangle {...triangleProps} />
-        {cp.showNodes && <Nodes {...curveProps} />}
       </SVG>
     </div>
   );
