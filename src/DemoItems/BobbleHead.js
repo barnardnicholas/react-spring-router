bc241333e8759e0ea8a8813af526824a972f98cb
Nick Barnard <barnard.nicholas@googlemail.com>
2021-11-28 20:46:38 +0000

    De-animated Bobbleheads & Home

--- src/DemoItems/BobbleHead.js
+++ src/DemoItems/BobbleHead.js
@@ -1,17 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useSpring, config, animated } from 'react-spring';
-import AnimatedButton from '../components/AnimatedButton';
-import useHeight from '../hooks/useHeight';
+import React from 'react';
 import rob from '../content/rob.png';
 import oli from '../content/oli.png';
 import mark from '../content/mark.png';
-import usePrevious from '../hooks/usePrevious';
-
-const customConfig = {
-  mass: 5,
-  tension: 800,
-  friction: 10,
-};
 
 const people = {
   rob,
@@ -24,11 +14,7 @@ const defaultSize = {
   height: 300,
 };
 
-const BobbleHead = ({ person = 'rob', extraStyles = {}, isAnimatingIn }) => {
-  const [props, setProps] = useState({ angle: 0, scale: 100 });
-  const [canMouseOver, setCanMouseOver] = useState(true);
-  const prevIsAnimatingIn = usePrevious(isAnimatingIn);
-
+const BobbleHead = ({ person = 'rob', extraStyles = {} }) => {
   const baseStyles = {
     ...defaultSize,
     transformOrigin: '50% 100%',
@@ -36,64 +22,11 @@ const BobbleHead = ({ person = 'rob', extraStyles = {}, isAnimatingIn }) => {
     ...extraStyles,
   };
 
-  const bobbleStyles = useSpring({
-    config: customConfig,
-    from: { transform: `rotate(0deg) scale(100%)` },
-    to: { transform: `rotate(${props.angle}deg) scale(${props.scale}%)` },
-  });
-
-  useEffect(() => {
-    if (!isAnimatingIn && prevIsAnimatingIn) _handleClick();
-  }, [isAnimatingIn, prevIsAnimatingIn]);
-
   return (
     <div className="bobble-container">
-      <animated.div
-        onClick={_handleClick}
-        onMouseEnter={_handleMouseEnter}
-        className="bobblehead"
-        style={{ ...baseStyles, ...bobbleStyles }}
-      />
+      <div className="bobblehead" style={baseStyles} />
     </div>
   );
-
-  function _handleClick() {
-    const angle = Math.random() * 20 - 10;
-    const scale = Math.random() * 40 - 20 + 100;
-    setProps({ angle, scale });
-    setTimeout(() => {
-      setProps({ angle: 0, scale: 100 });
-    }, 200);
-  }
-
-  function _handleMouseEnter(e) {
-    const { pageX, pageY } = e;
-    const { offsetWidth, offsetHeight, offsetTop, offsetLeft } = e.target;
-
-    const clientX = pageX - offsetLeft;
-    const clientY = pageY - offsetTop;
-
-    const isTop = clientY < offsetHeight / 2;
-    const isLeft = clientX < offsetWidth / 2;
-
-    let angle = 0;
-
-    angle += isLeft ? 10 : -10;
-    if (isTop) angle *= 1.2;
-
-    const scale = Math.random() * 40 - 20 + 100;
-
-    if (canMouseOver) {
-      setCanMouseOver(false);
-      setProps({ angle, scale });
-      setTimeout(() => {
-        setProps({ angle: 0, scale: 100 });
-      }, 200);
-      setTimeout(() => {
-        setCanMouseOver(true);
-      }, 500);
-    }
-  }
 };
 
 export default BobbleHead;
